27ada37a1ad8bc73214765125b1e22bc033dce4f
Laths <leonardo_soares@live.com>
2022-07-19 17:10:29 -0300

    make it better

--- fusion/settings.py
+++ fusion/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-kdza6$%!kf!s0(o%5$et92_xtdkw(=*=*3ii2$^@9k1!+o+$=#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['fusion-lsd.herokuapp.com']
 
 
 # Application definition
